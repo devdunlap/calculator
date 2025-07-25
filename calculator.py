5b6f2b58154d2ceb17c8763a42ef5a855388b1c7
devdunlap <dev.dunlap.andrew@gmail.com>
2025-07-24 23:17:08 -0500

    created calculator function

--- calculator.py
+++ calculator.py
@@ -17,4 +17,25 @@ operations = {
     "*": multiply,
     "/": divide
 }
-print(operations ["*"](4, 8))
+#print(operations ["*"](4, 8))
+def calculator():
+    num1 = float(input("Enter first number: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation:\n ")
+        num2 = float(input("Enter next number:\n "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+
+# Main program loop
+while True:
+    calculator()
+    if input("Type 'q' to quit or any other key to start a new calculation: ") == 'q':
+        break
