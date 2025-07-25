d66e1dc4270a9003d40e5e06a77649971bc11d3d
devdunlap <dev.dunlap.andrew@gmail.com>
2025-07-24 22:31:29 -0500

    defined logic

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def is_leap_year(year):
+    """Check if a year is a leap year."""
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    return False
+
+print(is_leap_year(2020))  # True
+print(is_leap_year(2021))  # False
